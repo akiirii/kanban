3db43614bc9baa0272f4cc265d54173dd1a4a898
akiirii <akiirii@gmail.com>
2015-10-04 21:57:28 +0200

    add edit ticked and button style

--- frontend/js/ticket.js
+++ frontend/js/ticket.js
@@ -8,6 +8,16 @@ angular
             },
             templateUrl: '/html/ticket.html',
             controller: ['$scope', function($scope) {
+                $scope.ticket.$editing = ''
+
+                $scope.editContent = function(element) {
+
+                    $scope.ticket.$editing = element;
+                };
+                $scope.save = function() {
+                    $scope.ticket.$editing = '';
+
+                };
 
             }]
         }
